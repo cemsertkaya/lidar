da6e4d57b7fcd962a056aaa88eb7d17dde50bea3
Cem Sertkaya <cemsertkaya99@gmail.com>
2024-05-05 13:28:38 +0300

    Initial Commit

---
+++ Lidar/LidarApp.swift
@@ -0,0 +1,17 @@
+//
+//  LidarApp.swift
+//  Lidar
+//
+//  Created by Cem Sertkaya on 5.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct LidarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
